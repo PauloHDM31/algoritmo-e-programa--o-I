3e4c3795894cee21ff9d51b67962b9e19fb9a904
Paulo <PAULOhdm236@gmail.com>
2024-09-22 17:09:22 -0300

    Resolução de exercício- questão 3

---
+++ Questao3-paresde1a500.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(){  
+    for ( int numeropar = 1 ; numeropar <=500; numeropar++){
+        if(numeropar%2==0){
+            printf("%d\t",numeropar);
+        }
+    }        
+}
