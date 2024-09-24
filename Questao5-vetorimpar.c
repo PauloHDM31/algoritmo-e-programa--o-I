f3040ee8f7708878c295ac44024a1901a1c47381
Paulo <PAULOhdm236@gmail.com>
2024-09-24 10:54:38 -0300

    Resolução de exercício- questão 5

---
+++ Questao5-vetorimpar.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <locale.h>
+
+int main(){
+    setlocale(LC_ALL,"Portuguese");
+    int vetorimpar[10];
+    int cont = 0;
+
+    for(int i = 0; i < 10; i++){
+        printf("Digite um número: ");
+            scanf("%d", &vetorimpar[i]);
+    }
+    for(int i = 0; i < 10; i++){
+        if(vetorimpar[i]%2==1){
+            printf("%d\t",vetorimpar[i]);
+        }
+    }
+    for(int i = 0; i < 10; i++){
+        if(vetorimpar[i]%2==1){
+            cont = cont+1;
+        }
+    }
+        printf("\nEste vetor apresenta %d valores ímpares.\n", cont);
+}
